5556dadca2159aba4d94c9ce07c4ce8d4e19b0c1
vishnupriyabalu <visnupriyabalu@gmail.com>
2017-08-21 16:10:33 +0530

    I have updated

--- FinalProject/src/Mainclass.java
+++ FinalProject/src/Mainclass.java
@@ -3,6 +3,7 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Mainclass {
+System.out.println("In main class");
 	Scanner sc=new Scanner(System.in);
 	public static ArrayList<Product> ll ;
 	
